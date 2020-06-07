94f202d3366c0152d9ddb6c1d7dc5a714f1949c2
Dmitry Karyakin <skycrazyk@gmail.com>
2020-06-07 21:17:30 +0500

    back up

--- src/components/Groups/index.tsx
+++ src/components/Groups/index.tsx
@@ -49,9 +49,54 @@ const Groups: FC = () => {
                     rules={[rules.reuired]}
                   >
                     <Input placeholder="Название группы" />
-                  </Form.Item>
 
-                  {/* <Form.List name="fields"></Form.List> */}
+                    <Form.List name="fields">
+                      {(
+                        groupValues,
+                        { add: addGroupValue, remove: removeGroupValue }
+                      ) => {
+                        return (
+                          <div>
+                            {groupValues.map((groupValue) => (
+                              <Space
+                                key={groupValue.key}
+                                style={{ display: 'flex', marginBottom: 8 }}
+                                align="start"
+                              >
+                                <Form.Item
+                                  {...groupValue}
+                                  name={[groupValue.name, 'name']}
+                                  fieldKey={[groupValue.fieldKey, 'name']}
+                                  rules={[rules.reuired]}
+                                >
+                                  <Input placeholder="Значение" />
+                                </Form.Item>
+
+                                <MinusCircleOutlined
+                                  className={style.deleteButton}
+                                  onClick={() => {
+                                    removeGroupValue(groupValue.name);
+                                  }}
+                                />
+                              </Space>
+                            ))}
+
+                            <Form.Item>
+                              <Button
+                                type="dashed"
+                                onClick={() => {
+                                  addGroupValue();
+                                }}
+                                block
+                              >
+                                <PlusOutlined /> Добавить значение
+                              </Button>
+                            </Form.Item>
+                          </div>
+                        );
+                      }}
+                    </Form.List>
+                  </Form.Item>
 
                   <MinusCircleOutlined
                     className={style.deleteButton}
