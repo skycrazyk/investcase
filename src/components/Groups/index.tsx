676f5c5bf4a0763fe2c828be818d71d8b684eae3
Dmitry Karyakin <skycrazyk@gmail.com>
2020-06-07 19:23:34 +0500

    back up

--- src/components/Groups/index.tsx
+++ src/components/Groups/index.tsx
@@ -1,11 +1,87 @@
-import React, { FC } from 'react';
-import { Button, Input, Form, Select } from 'antd';
+import React, { FC, useState } from 'react';
+import { Button, Input, Form, Select, Space } from 'antd';
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
+import { useSelector, useDispatch } from 'react-redux';
+import { nanoid } from '@reduxjs/toolkit';
+import { groupsSelectors, groupsActions } from '../../store/groups';
+import style from './style.module.css';
+
+const rules = {
+  reuired: { required: true, message: 'Обязательное поле' },
+};
 
 const Groups: FC = () => {
+  const dispatch = useDispatch();
+  const groups = useSelector(groupsSelectors.selectAll);
+
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
-    <Form>
-      <Button>Сохранить</Button>
-      <Button>Добавить</Button>
+    <Form
+      name="groups"
+      initialValues={{
+        groups: [{ name: 'Валюта' }, { name: 'Уровень риска' }],
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.List name="groups">
+        {(groups, { add, remove }) => {
+          return (
+            <div>
+              {groups.map((group) => (
+                <Space
+                  key={group.key}
+                  style={{ display: 'flex', marginBottom: 8 }}
+                  align="start"
+                >
+                  <Form.Item
+                    {...group}
+                    name={[group.name, 'name']}
+                    fieldKey={[group.fieldKey, 'name']}
+                    rules={[rules.reuired]}
+                  >
+                    <Input placeholder="Название группы" />
+                  </Form.Item>
+
+                  {/* <Form.List name="fields"></Form.List> */}
+
+                  <MinusCircleOutlined
+                    className={style.deleteButton}
+                    onClick={() => {
+                      remove(group.name);
+                    }}
+                  />
+                </Space>
+              ))}
+
+              <Form.Item>
+                <Button
+                  type="dashed"
+                  onClick={() => {
+                    add();
+                  }}
+                  block
+                >
+                  <PlusOutlined /> Добавить группу
+                </Button>
+              </Form.Item>
+            </div>
+          );
+        }}
+      </Form.List>
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Сохранить
+        </Button>
+      </Form.Item>
     </Form>
   );
 };
