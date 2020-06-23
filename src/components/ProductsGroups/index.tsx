400efd9a1dd0f865da45039d7e23f312435f503b
Dmitry Karyakin <skycrazyk@gmail.com>
2020-06-24 00:02:01 +0500

    Добавлен селект с группами

--- src/components/ProductsGroups/index.tsx
+++ src/components/ProductsGroups/index.tsx
@@ -1,73 +1,33 @@
 import React, { FC } from 'react';
-import { Form, Select, Button, Space } from 'antd';
-import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
+import { Form, Select } from 'antd';
 import { useSelector, useDispatch } from 'react-redux';
 import { groupsSelectors } from '../../store/groups';
 import { productsActions, productsSelectors } from '../../store/products';
 
 const ProductsGroups: FC = () => {
   const dispatch = useDispatch();
+  const [form] = Form.useForm();
   const groupsCatalog = useSelector(groupsSelectors.selectAll);
+  const storeGroups = useSelector(productsSelectors.getGroups);
 
   return (
     <Form
+      form={form}
       name="groups"
+      initialValues={{ groups: storeGroups }}
       onValuesChange={(changedValues, values) => {
         dispatch(productsActions.setGroups(values.groups));
       }}
     >
-      <Form.List name="groups">
-        {(fields, { add, remove }) => {
-          return (
-            <div>
-              {fields.map((field, index) => (
-                <Form.Item required={false} key={field.key}>
-                  <Form.Item
-                    {...field}
-                    validateTrigger={['onChange', 'onBlur']}
-                    rules={[
-                      {
-                        required: true,
-                        whitespace: true,
-                        message: 'Выберите группу или удалите группировку',
-                      },
-                    ]}
-                    noStyle
-                  >
-                    <Select>
-                      {groupsCatalog.map((group) => (
-                        <Select.Option value={group.id}>
-                          {group.name}
-                        </Select.Option>
-                      ))}
-                    </Select>
-                  </Form.Item>
-                  {fields.length > 1 ? (
-                    <MinusCircleOutlined
-                      className="dynamic-delete-button"
-                      style={{ margin: '0 8px' }}
-                      onClick={() => {
-                        remove(field.name);
-                      }}
-                    />
-                  ) : null}
-                </Form.Item>
-              ))}
-              <Form.Item>
-                <Button
-                  type="dashed"
-                  onClick={() => {
-                    add();
-                  }}
-                  style={{ width: '60%' }}
-                >
-                  <PlusOutlined /> Add field
-                </Button>
-              </Form.Item>
-            </div>
-          );
-        }}
-      </Form.List>
+      <Form.Item name="groups">
+        <Select mode="multiple" placeholder="Группировка продуктов" allowClear>
+          {groupsCatalog.map((group) => (
+            <Select.Option key={group.id} value={group.id}>
+              {group.name}
+            </Select.Option>
+          ))}
+        </Select>
+      </Form.Item>
     </Form>
   );
 };
