7af7bc08c278b15cea180887e69bc47a9c922fd7
Dmitry Karyakin <skycrazyk@gmail.com>
2020-06-27 00:53:16 +0500

    Добавлена группировка продуктов

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useCallback, useState } from 'react';
-import { Button, Space, Table } from 'antd';
+import { Button } from 'antd';
 import { nanoid } from '@reduxjs/toolkit';
 import { useSelector, useDispatch } from 'react-redux';
 import {
@@ -15,9 +15,7 @@ import ProductsTable from '../ProductsTable';
 
 const Products: FC = () => {
   const dispatch = useDispatch();
-  const products = useSelector(productsSelectors.selectAll);
   const productsEntities = useSelector(productsSelectors.selectEntities);
-  const dataSource = products.map((item) => ({ ...item, key: item.id }));
   const createModal = useModalActions();
   const editModal = useModalActions();
   const [editableProduct, setEditableProduct] = useState<TProduct>();
@@ -31,37 +29,6 @@ const Products: FC = () => {
     dispatch(productsActions.removeOne(id));
   };
 
-  const columns = [
-    {
-      title: 'Название',
-      dataIndex: 'name',
-      key: 'name',
-    },
-    {
-      title: 'Тикер',
-      dataIndex: 'ticker',
-      key: 'ticker',
-    },
-    {
-      title: 'Валюта покупки',
-      dataIndex: 'currency',
-      key: 'currency',
-      render: (currency: string) => currency.toUpperCase(),
-    },
-    {
-      title: 'Действия',
-      key: 'action',
-      render: (text: any, product: TProduct) => {
-        return (
-          <Space size="middle">
-            <a onClick={() => editProduct(product.id)}>Изменить</a>
-            <a onClick={() => deleteProduct(product.id)}>Удалить</a>
-          </Space>
-        );
-      },
-    },
-  ];
-
   return (
     <>
       <PageHeader
@@ -72,8 +39,7 @@ const Products: FC = () => {
         ]}
       />
       <ProductsGroups />
-      <ProductsTable />
-      <Table columns={columns} dataSource={dataSource} pagination={false} />
+      <ProductsTable editProduct={editProduct} deleteProduct={deleteProduct} />
       <Product
         initialValues={{ id: nanoid() }}
         title="Создание продукта"
--- src/components/ProductsTable/index.tsx
+++ src/components/ProductsTable/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, ReactElement } from 'react';
 import { useSelector } from 'react-redux';
 import { Dictionary } from '@reduxjs/toolkit';
 import { Table, Space } from 'antd';
@@ -11,122 +11,40 @@ import {
 
 import { groupsSelectors, TGroup, TValue } from '../../store/groups';
 
-const groupedProducts = (
-  restProductsGroups: TProductsGroups,
-  allProductsGroups: TProductsGroups,
-  products: TProduct[],
-  groups: Dictionary<TGroup>,
-  parentGroup?: TGroup | undefined,
-  parentGroupValue?: TValue | undefined
-) => {
-  const copyProductsGroups = [...restProductsGroups];
-  const currentGroupId = copyProductsGroups.shift();
-  const currentGroup = groups[currentGroupId || ''];
-
-  const columns = currentGroup
-    ? [
-        {
-          title: currentGroup.name,
-          dataIndex: 'name',
-          key: 'name',
-        },
-      ]
-    : [
-        {
-          title: 'Название продукта',
-          dataIndex: 'name',
-          key: 'name',
-        },
-        {
-          title: 'Тикер',
-          dataIndex: 'ticker',
-          key: 'ticker',
-        },
-        {
-          title: 'Валюта покупки',
-          dataIndex: 'currency',
-          key: 'currency',
-          render: (currency: string) => currency.toUpperCase(),
-        },
-        // {
-        //   title: 'Действия',
-        //   key: 'action',
-        //   render: (text: any, product: TProduct) => {
-        //     return (
-        //       <Space size="middle">
-        //         <a onClick={() => editProduct(product.id)}>Изменить</a>
-        //         <a onClick={() => deleteProduct(product.id)}>Удалить</a>
-        //       </Space>
-        //     );
-        //   },
-        // },
-      ];
-
-  const filteredProducts =
-    parentGroup && parentGroupValue
-      ? products.filter((product) => {
-          return product.groups[parentGroup.id] === parentGroupValue.id;
-        })
-      : products;
-
-  const dataSource = currentGroup
-    ? currentGroup.values.map((item) => ({ ...item, key: item.id }))
-    : filteredProducts.map((item) => ({ ...item, key: item.id }));
-
-  const expandable = currentGroup
-    ? {
-        expandedRowRender: (record: TValue) =>
-          groupedProducts(
-            copyProductsGroups,
-            allProductsGroups,
-            filteredProducts,
-            groups,
-            currentGroup,
-            record
-          ),
-      }
-    : undefined;
-
-  return (
-    <Table
-      columns={columns}
-      dataSource={dataSource}
-      pagination={false}
-      expandable={expandable}
-    />
-  );
-};
-
 const nodeTypes = {
   group: 'group',
   products: 'products',
 } as const;
 
-type GroupedProducts =
-  | {
-      type: typeof nodeTypes.group;
-      group: TGroup;
-      parent?: {
-        group: TGroup;
-        /** undefined в случае если есть продуты без значения в текущей группе */
-        groupValue?: TValue;
-      };
-      children?: GroupedProducts[];
-    }
-  | {
-      type: typeof nodeTypes.products;
-      products: TProduct[];
-    };
+type TGroupNodeValue = {
+  value?: TValue;
+  child: TGroupedProducts;
+};
 
+type TGroupNode = {
+  type: typeof nodeTypes.group;
+  group: TGroup;
+  values: TGroupNodeValue[];
+};
+
+type TProductsNode = {
+  type: typeof nodeTypes.products;
+  products: TProduct[];
+};
+
+type TGroupedProducts = TGroupNode | TProductsNode;
+
+/**
+ * Формирурует дерево продуктов согласно списоку идентификаторов групп
+ * @param productsGroupsIds Список идентификаторов групп для фильтрации
+ * @param groupsEntities Каталог групп (по id)
+ * @param productsCatalog Каталог продуктов
+ */
 const groupProducts = (
   productsGroupsIds: TProductsGroups,
   groupsEntities: Dictionary<TGroup>,
-  productsCatalog: TProduct[],
-  parent?: {
-    group: TGroup;
-    groupValue?: TValue;
-  }
-): GroupedProducts => {
+  productsCatalog: TProduct[]
+): TGroupedProducts => {
   const copyProductsGroupsIds = [...productsGroupsIds];
   const currentGroupId = copyProductsGroupsIds.shift();
   const currentGroup = currentGroupId && groupsEntities[currentGroupId];
@@ -139,14 +57,14 @@ const groupProducts = (
         );
 
         if (filteredProducts.length) {
-          acc.push(
-            groupProducts(
+          acc.push({
+            value: groupValue,
+            child: groupProducts(
               copyProductsGroupsIds,
               groupsEntities,
-              filteredProducts,
-              { group: currentGroup, groupValue }
-            )
-          );
+              filteredProducts
+            ),
+          });
         }
 
         return acc;
@@ -164,18 +82,19 @@ const groupProducts = (
     });
 
     // Продукты без значения в текущей группе собираются в отдельном узле
-    const unfilteredChild =
-      Boolean(unfilteredProducts.length) &&
-      groupProducts(copyProductsGroupsIds, groupsEntities, unfilteredProducts, {
-        group: currentGroup,
-        groupValue: undefined,
-      });
+    const unfilteredChild = Boolean(unfilteredProducts.length) && {
+      value: undefined,
+      child: groupProducts(
+        copyProductsGroupsIds,
+        groupsEntities,
+        unfilteredProducts
+      ),
+    };
 
     return {
-      parent,
       type: nodeTypes.group,
       group: currentGroup,
-      children: [
+      values: [
         ...filteredChildren,
         ...(unfilteredChild ? [unfilteredChild] : []),
       ],
@@ -188,19 +107,130 @@ const groupProducts = (
   }
 };
 
-const ProductsTable: FC = () => {
+/**
+ * Рекурсивно рендереит дерево продуктов
+ * @param groupedProducts
+ */
+const treeNextProducts = ({
+  groupedProducts,
+  editProduct,
+  deleteProduct,
+}: {
+  groupedProducts: TGroupedProducts;
+  editProduct: (id: string) => void;
+  deleteProduct: (id: string) => void;
+}) => {
+  let result: ReactElement;
+
+  if (groupedProducts.type === nodeTypes.group) {
+    const columns = [
+      {
+        title: groupedProducts.group.name,
+        dataIndex: ['value', 'name'],
+      },
+    ];
+
+    const data = groupedProducts.values.map((item) => {
+      let resolvedValue;
+
+      if (item.value) {
+        resolvedValue = {
+          ...item,
+          key: item.value.id,
+        };
+      }
+      // Случай с продуктами без значения в текущей группе
+      else {
+        resolvedValue = {
+          ...item,
+          value: {
+            name: `Без значения в группе "${groupedProducts.group.name}"`,
+            id: 'unvalued',
+          },
+          key: 'unvalued',
+        };
+      }
+
+      return resolvedValue;
+    });
+
+    const expandable = {
+      expandedRowRender: (record: TGroupNodeValue) =>
+        treeNextProducts({
+          groupedProducts: record.child,
+          editProduct,
+          deleteProduct,
+        }),
+    };
+
+    result = (
+      <Table
+        columns={columns}
+        dataSource={data}
+        pagination={false}
+        expandable={expandable}
+      />
+    );
+  } else {
+    const columns = [
+      {
+        title: 'Название продукта',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: 'Тикер',
+        dataIndex: 'ticker',
+        key: 'ticker',
+      },
+      {
+        title: 'Валюта покупки',
+        dataIndex: 'currency',
+        key: 'currency',
+        render: (currency: string) => currency.toUpperCase(),
+      },
+      {
+        title: 'Действия',
+        key: 'action',
+        render: (text: any, product: TProduct) => {
+          return (
+            <Space size="middle">
+              <a onClick={() => editProduct(product.id)}>Изменить</a>
+              <a onClick={() => deleteProduct(product.id)}>Удалить</a>
+            </Space>
+          );
+        },
+      },
+    ];
+
+    const data = groupedProducts.products.map((item) => ({
+      ...item,
+      key: item.id,
+    }));
+
+    result = <Table columns={columns} dataSource={data} pagination={false} />;
+  }
+
+  return result;
+};
+
+type TProductTable = {
+  editProduct: (id: string) => void;
+  deleteProduct: (id: string) => void;
+};
+
+const ProductsTable: FC<TProductTable> = ({ editProduct, deleteProduct }) => {
   const productsGroups = useSelector(productsSelectors.getGroups);
   const productsCatalog = useSelector(productsSelectors.selectAll);
   const groupsEntities = useSelector(groupsSelectors.selectEntities);
 
-  console.log(groupProducts(productsGroups, groupsEntities, productsCatalog));
-
-  return groupedProducts(
+  const groupedProducts = groupProducts(
     productsGroups,
-    productsGroups,
-    productsCatalog,
-    groupsEntities
+    groupsEntities,
+    productsCatalog
   );
+
+  return treeNextProducts({ groupedProducts, editProduct, deleteProduct });
 };
 
 export default ProductsTable;
