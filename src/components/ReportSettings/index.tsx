0f1033de63881c109ae161f8f5b864fdb7e7b3d3
Dmitry Karyakin <skycrazyk@gmail.com>
2020-07-07 00:17:37 +0500

    Report settings as form

--- src/components/ReportSettings/index.tsx
+++ src/components/ReportSettings/index.tsx
@@ -1,23 +1,35 @@
 import React, { FC } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Select } from 'antd';
 import { Store } from 'antd/lib/form/interface';
 import { reportsActions, reportsSelectors } from '../../store/reports';
-import GroupsFilter from '../GroupsFilter';
+import { groupsSelectors } from '../../store/groups';
 
 const ReportSettings: FC = () => {
   const dispatch = useDispatch();
+  const groupsCatalog = useSelector(groupsSelectors.selectAll);
+  const settings = useSelector(reportsSelectors.getSettings);
 
-  const setGroups = (changedValues: Store, values: Store) => {
-    dispatch(reportsActions.setGroups(values.groups));
+  const onSettingsChange = (changedValues: Store, values: Store) => {
+    dispatch(reportsActions.setSettings(values));
   };
 
   return (
-    <>
-      <GroupsFilter
-        onChange={setGroups}
-        groupsSelector={reportsSelectors.getGroups}
-      />
-    </>
+    <Form
+      name="settings"
+      initialValues={settings}
+      onValuesChange={onSettingsChange}
+    >
+      <Form.Item name="groups">
+        <Select mode="multiple" placeholder="Группировка продуктов" allowClear>
+          {groupsCatalog.map((group) => (
+            <Select.Option key={group.id} value={group.id}>
+              {group.name}
+            </Select.Option>
+          ))}
+        </Select>
+      </Form.Item>
+    </Form>
   );
 };
 
