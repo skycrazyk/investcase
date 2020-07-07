e7cfe6f7b24d5530b93e3bf26c6008df48c757b8
Dmitry Karyakin <skycrazyk@gmail.com>
2020-07-08 00:12:50 +0500

    автоматическая установка периода для сравнения

--- src/components/ReportSettings/index.tsx
+++ src/components/ReportSettings/index.tsx
@@ -3,11 +3,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Form, Select, Collapse } from 'antd';
 import { Store } from 'antd/lib/form/interface';
 import { useParams } from 'react-router-dom';
-import { reportsActions, reportsSelectors } from '../../store/reports';
+import moment from 'moment';
+import { reportsActions, reportsSelectors, TReport } from '../../store/reports';
 import { groupsSelectors } from '../../store/groups';
+import { State } from '../../store';
 
 const ReportSettings: FC = () => {
   const dispatch = useDispatch();
+  const [form] = Form.useForm();
   const routeParams = useParams<{ id: string }>();
   const groupsCatalog = useSelector(groupsSelectors.selectAll);
   const settings = useSelector(reportsSelectors.getSettings);
@@ -17,15 +20,47 @@ const ReportSettings: FC = () => {
     dispatch(reportsActions.setSettings(values));
   };
 
+  const currentReport = useSelector((state: State) =>
+    reportsSelectors.selectById(state, routeParams.id)
+  );
+
+  // Устанавливаем период для сравнения
   useEffect(() => {
-    // TODO: setup previos report for compare
-    console.log(routeParams.id);
+    if (!currentReport) return;
+
+    const currentReportDate = moment(currentReport.date);
+
+    const oldestReports = reports.filter((report) =>
+      currentReportDate.isAfter(report.date)
+    );
+
+    if (!oldestReports.length) {
+      form.setFieldsValue({ compareReportId: undefined });
+      // BUG: onSettingsChange не вызывается автоматически
+      dispatch(reportsActions.setSettings({ compareReportId: undefined }));
+      return;
+    }
+
+    const previousReport = oldestReports.reduce((oldestReport, report) => {
+      if (moment(oldestReport.date).isBefore(report.date)) {
+        return report;
+      }
+
+      return oldestReport;
+    });
+
+    form.setFieldsValue({ compareReportId: previousReport.id });
+    // BUG: onSettingsChange не вызывается автоматически
+    dispatch(
+      reportsActions.setSettings({ compareReportId: previousReport.id })
+    );
   }, [routeParams.id]);
 
   return (
     <Collapse defaultActiveKey={['1']} style={{ marginBottom: 24 }}>
       <Collapse.Panel key="1" header="Настройки">
         <Form
+          form={form}
           layout="inline"
           name="settings"
           initialValues={settings}
