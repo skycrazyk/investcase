431484cd4df54bc6d8b7116b11481b7d5f70d4ab
Dmitry Karyakin <skycrazyk@gmail.com>
2020-07-16 22:01:08 +0500

    update comment

--- src/selectors/index.ts
+++ src/selectors/index.ts
@@ -1,4 +1,4 @@
-// export { default as getDownloadData } from './getDownloadData';
+// export { default as getDownloadData } from './getDownloadData'; // TODO: раскоментировать после переноса getGroups, getReports и getProducts в соответствующие сторы
 export { default as getGroups } from './getGroups';
 export { default as getReports } from './getReports';
 export { default as getProducts } from './getProducts';
